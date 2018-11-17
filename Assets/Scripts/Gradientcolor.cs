1aca5133a8e94612928030fc3217796ace379430
justinienghorra <justinienghorra@hotmail.fr>
2018-11-17 17:33:27 +0100

    Assets commit

---
+++ Assets/Scripts/Gradientcolor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gradientcolor : MonoBehaviour {
+
+	public Color startColor;
+	public Color endColor;
+	
+	// Use this for initialization
+	void Start () {
+		var mesh = GetComponent<MeshFilter>().mesh;
+		var uv = mesh.uv;
+		var colors = new Color[uv.Length];
+
+		// Instead if vertex.y we use uv.x
+		for (var i = 0; i < uv.Length;i++)
+			colors[i] = Color.Lerp(Color.white, Color.white, uv[i].x); 
+
+		mesh.colors = colors;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
