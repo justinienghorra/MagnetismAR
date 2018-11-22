a0979960864fde59578f26e6f5d63e8b14a67211
justinienghorra <justinienghorra@hotmail.fr>
2018-11-22 19:00:42 +0100

    Final commit : delays, success animation, help

---
+++ Assets/Scripts/Help.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Help : MonoBehaviour, Vuforia.ITrackableEventHandler {
+
+	private TrackableBehaviour mTrackableBehaviour;
+	
+	public GameObject sparks;
+
+	// Use this for initialization
+	void Start () {
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour) {
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+		
+		sparks.SetActive(false);
+	}
+	
+	public void OnTrackableStateChanged (TrackableBehaviour.Status newStatus) {
+        OnTrackableStateChanged (default(TrackableBehaviour.Status), newStatus);
+    }
+	
+	public void OnTrackableStateChanged(
+         TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus) {
+         if (newStatus == TrackableBehaviour.Status.DETECTED ||
+             newStatus == TrackableBehaviour.Status.TRACKED ||
+             newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+            sparks.SetActive(true);
+        } else {
+			sparks.SetActive(false);
+		}
+    }  
+	
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -13,23 +13,42 @@ public class Interaction : MonoBehaviour {
 	public GameObject magnetFran;
 	public GameObject barFran;
 	
-	public GameObject key;
+	public GameObject key;	
 	
 	public float franToMagnet, franToBar;
 	
-	public bool gotMagnet, gotBar;
+	private bool gotMagnet, gotBar;
 	
-	//public Text test;
+	private bool success, jump;
+	private int jumpCount;
+	
+	private bool barAttracted, keyAttracted;
 
-	// Use this for initialization
+	IEnumerator TimerKey() {
+		yield return new WaitForSeconds(1.0f);
+		keyAttracted = true;
+	}
+	
+	IEnumerator TimerBar() {
+		yield return new WaitForSeconds(0.5f);
+		barAttracted = true;
+	}
+
 	void Start () {
 		gotMagnet = false;
 		gotBar = false;
+		success = false;
+		
+		barAttracted = false;
+		keyAttracted = false;
+		
 		magnetFran.SetActive(false);
 		barFran.SetActive(false);
+		
+		jump = false;
+		jumpCount = 0;
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		Vector3 posF = fran.transform.position;	
 		Vector3 posM = magnetDefault.transform.position;
@@ -38,21 +57,58 @@ public class Interaction : MonoBehaviour {
 		franToMagnet = Vector3.Distance(posM, posF);
 		franToBar = Vector3.Distance(posB, posF);
 		
-		if (gotMagnet || franToMagnet < 1.7) {
+		if(success) {
+			if(posF.y < -3.46) {
+				fran.transform.position = new Vector3(posF.x-0.008f, posF.y+0.02f, posF.z);
+			} else if(jumpCount < 10) {
+				if(!jump && posF.z < 1.355f) {
+					fran.transform.position = new Vector3(posF.x, posF.y, posF.z+0.005f);
+				} else {
+					if(posF.z > 1.345f) {
+						jump = true;
+						fran.transform.position = new Vector3(posF.x, posF.y, posF.z-0.005f);
+					} else {
+						jump = false;
+						jumpCount++;
+					}
+				}
+			}
+		} else if (!success && (gotMagnet || franToMagnet < 2.4)) {
 			gotMagnet = true;
 			magnetFran.SetActive(true);
 			magnetDefault.SetActive(false);
 			
-			if (gotBar || franToBar < 1.7) {
+			if (gotBar || franToBar < 2.3) {
 				gotBar = true;
 				barFran.SetActive(true);
 				barDefault.SetActive(false);
 				
-				Vector3 posK = key.transform.localPosition;
-				//test.text = key.transform.localPosition.x.ToString("R");
-				if (posK.x > 1.97F){
-					key.transform.localPosition = new Vector3(posK.x - 0.02F, posK.y, posK.z);
-				}	
+				if(!barAttracted) {
+					StartCoroutine(TimerBar());
+				} else {
+					Vector3 posK = key.transform.localPosition;
+					Vector3 rotK = key.transform.eulerAngles;
+					
+					if (posK.x > 3.3f){
+						key.transform.localPosition = new Vector3(posK.x - 0.0225f, posK.y, posK.z + 0.01f);
+						key.transform.eulerAngles = new Vector3(rotK.x, rotK.y - 2, rotK.z);
+					} else {
+						if(!keyAttracted) {
+							StartCoroutine(TimerKey());
+						} else {
+							key.SetActive(false);
+							barFran.SetActive(false);
+							magnetFran.SetActive(false);
+							if (posF.y > -5.0f && !success) {
+								fran.transform.position = new Vector3(posF.x, posF.y-0.02f, posF.z);
+							} else if(!success) {
+								success = true;
+								Vector3 localPosF = fran.transform.localPosition;
+								fran.transform.localPosition = new Vector3(4.7f, localPosF.y, localPosF.z);
+							}
+						}
+					}
+				}				
 			}
 		}
 	}
